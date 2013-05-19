db778e941ab3aba07bcad475ef6c948c8c94d779
Johannes Rudolph <johannes.rudolph@gmx.com>
2013-05-19 20:56:43 +0200

    Klassen hinzugefügt
    Es zeigt sich, dass es sinvoll ist GPS UTM und UTMREF zu trennen

---
+++ UTMREF.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: johannes
+ * Date: 19.05.13
+ * Time: 20:55
+ * To change this template use File | Settings | File Templates.
+ */
+
+class UTMREF {
+
+}
